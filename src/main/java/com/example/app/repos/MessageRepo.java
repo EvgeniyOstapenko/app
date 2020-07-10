f2f507eb8295eb55898a3c3410372e3579b2a9ed
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-07-10 21:33:20 +0300

    Initial commit

---
+++ src/main/java/com/example/app/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.app.repos;
+
+import com.example.app.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
