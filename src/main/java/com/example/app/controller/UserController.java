ecf06cb21348689cea9d409c5669c12037efe5bd
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-07-14 12:11:40 +0300

    App_6.2

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -4,24 +4,25 @@ import com.example.app.domain.Role;
 import com.example.app.domain.User;
 import com.example.app.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepo userRepo;
 
     @GetMapping
-    public String userList(Model model){
+    public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
         return "userList";
     }
@@ -34,30 +35,28 @@ public class UserController {
         return "userEdit";
     }
 
-    @PostMapping("{userId}")
+    @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam Long userId
-    ){
-
+    ) {
         User user = userRepo.findById(userId).orElse(null);
         user.setUsername(username);
-        toAddUserRoles(form, user);
+
+        addRolesToUser(form, user);
 
         userRepo.save(user);
         return "redirect:/user";
     }
 
-    private static void toAddUserRoles(@RequestParam Map<String, String> form, @RequestParam("userId") User user) {
-        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+    private static void addRolesToUser(@RequestParam Map<String, String> form, User user) {
+        Set<String> allPossibleRoles = Arrays.stream(Role.values())
+                .map(Role::name).collect(Collectors.toSet());
 
         user.getRoles().clear();
 
-        for(String key : form.keySet()){
-            if(roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
+        form.keySet().stream().filter(allPossibleRoles::contains)
+                .forEach(newKey -> user.getRoles().add(Role.valueOf(newKey)));
     }
 }
