ca47fcb99c443a4d79ff2698729e915f9e64721a
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-07-14 18:51:10 +0300

    App_7.1

--- src/main/java/com/example/app/domain/Message.java
+++ src/main/java/com/example/app/domain/Message.java
@@ -16,6 +16,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message() {
     }
 
@@ -59,5 +61,13 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
