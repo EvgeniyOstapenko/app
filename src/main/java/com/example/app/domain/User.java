230f2b0c0c7a62fc6d91487aca7b2525e9a79603
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-07-15 18:51:49 +0300

    App_8.1

--- src/main/java/com/example/app/domain/User.java
+++ src/main/java/com/example/app/domain/User.java
@@ -37,6 +37,10 @@ public class User implements UserDetails {
         return username;
     }
 
+    public boolean isAdmin(){
+        return roles.contains(Role.ADMIN);
+    }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
