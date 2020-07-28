403b83aa051f24d6ec2d37c110f8999f7ebbb9f4
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-07-28 12:29:45 +0300

    testing

---
+++ src/test/java/com/example/app/LoginTest.java
@@ -0,0 +1,32 @@
+package com.example.app;
+
+import com.example.app.controller.MainController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginText {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController controller;
+
+    @Test
+    public void test() throws Exception{
+//        assertThat(controller).isNotNull();
+
+        this.mockMvc.perform(get("/"))
+                .andDo()
+    }
+}
