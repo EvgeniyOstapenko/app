0a88580b3de26b6691223cb46c0391f6ad963520
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-11-14 21:08:27 +0300

    App_master_hostname

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+
+
+
+
+
     }
 }
